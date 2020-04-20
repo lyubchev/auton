b84845ac27cf1eab65ed0cd2f4cc9bd17cd4287e
Lyubo Lyubchev <lyubo_2317@abv.bg>
2020-04-20 15:57:07 +0300

    Export youtube to external package

---
+++ autone/youtube/youtube.go
@@ -0,0 +1,68 @@
+package youtube
+
+import (
+	"context"
+	"errors"
+
+	"google.golang.org/api/option"
+	youtube "google.golang.org/api/youtube/v3"
+)
+
+type Order string
+
+const (
+	OrderRelevance Order = "relevance"
+	OrderTime      Order = "time"
+)
+
+type Client struct {
+	apiKey string
+}
+
+func New(apiKey string) *Client {
+	c := Client{apiKey: apiKey}
+	return &c
+}
+
+func (c *Client) GetComments(videoId string, order Order, maxComments int) ([]string, error) {
+
+	comments := []string{}
+
+	ctx := context.Background()
+	ctx, cancelCtxFunc := context.WithCancel(ctx)
+
+	youtubeService, err := youtube.NewService(ctx, option.WithAPIKey(c.apiKey))
+	if err != nil {
+		cancelCtxFunc()
+		return nil, err
+	}
+
+	commentThreadsService := youtube.NewCommentThreadsService(youtubeService)
+
+	// Get the instance with which we can do api calls to the youtube api
+	apiCall := commentThreadsService.List("snippet")
+
+	apiCall.TextFormat("plainText")
+	apiCall.VideoId(videoId)
+	apiCall.Order(string(order))
+
+	err = apiCall.Pages(ctx, func(resp *youtube.CommentThreadListResponse) error {
+		for _, item := range resp.Items {
+			c := item.Snippet.TopLevelComment.Snippet.TextDisplay
+			comments = append(comments, c)
+
+			if len(comments) == maxComments {
+				cancelCtxFunc()
+				break
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil && !errors.Is(err, context.Canceled) {
+		return nil, err
+	}
+
+	return comments, nil
+}
--- main.go
+++ main.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"log"
 	"os"
 
 	"github.com/davecgh/go-spew/spew"
+	"github.com/impzero/autone/autone/youtube"
 	"github.com/joho/godotenv"
-	"google.golang.org/api/option"
-
-	youtube "google.golang.org/api/youtube/v3"
 )
-
-const MaxComments = 500
 
 func main() {
 	err := godotenv.Load()
@@ -23,40 +17,9 @@ func main() {
 
 	GoogleApiKey := os.Getenv("GOOGLE_API_KEY")
 
-	comments := []string{}
-
-	ctx := context.Background()
-	ctx, cancelCtxFunc := context.WithCancel(ctx)
-
-	youtubeService, err := youtube.NewService(ctx, option.WithAPIKey(GoogleApiKey))
+	youtubeClient := youtube.New(GoogleApiKey)
+	comments, err := youtubeClient.GetComments("xvZqHgFz51I", youtube.OrderRelevance, 100)
 	if err != nil {
-		panic(err)
-	}
-
-	commentThreadsService := youtube.NewCommentThreadsService(youtubeService)
-
-	// Get the instance with which we can do api calls to the youtube api
-	apiCall := commentThreadsService.List("snippet")
-
-	apiCall.TextFormat("plainText")
-	apiCall.VideoId("1DfbhdG0tEk")
-	apiCall.Order("relevance")
-
-	err = apiCall.Pages(ctx, func(resp *youtube.CommentThreadListResponse) error {
-		for _, item := range resp.Items {
-			c := item.Snippet.TopLevelComment.Snippet.TextDisplay
-			comments = append(comments, c)
-
-			if len(comments) == MaxComments {
-				cancelCtxFunc()
-				break
-			}
-		}
-
-		return nil
-	})
-
-	if err != nil && !errors.Is(err, context.Canceled) {
 		panic(err)
 	}
 
