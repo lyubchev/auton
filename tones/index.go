674cdf2491b7e4f9dd6aae14d0b3371b4726396d
Lyubo Lyubchev <lyubo_2317@abv.bg>
2020-04-21 22:34:53 +0300

    make tones separate package

---
+++ tones/index.go
@@ -0,0 +1,13 @@
+package tones
+
+type Tone string
+
+const (
+	Anger      Tone = "Anger"
+	Fear       Tone = "Fear"
+	Joy        Tone = "Joy"
+	Sadness    Tone = "Sadness"
+	Analytical Tone = "Analytical"
+	Confident  Tone = "Confident"
+	Tentative  Tone = "Tentative"
+)
