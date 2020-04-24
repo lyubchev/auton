229fe276097ecdd457cdcaba54bb8c29cb0bf69c
Lyubo Lyubchev <lyubo_2317@abv.bg>
2020-04-24 18:42:30 +0300

    setup some middlewares (cors)

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/IBM/go-sdk-core v0.0.0-20200217212347-fe152a0e9e89
 	github.com/davecgh/go-spew v1.1.1
 	github.com/go-chi/chi v4.1.1+incompatible
+	github.com/go-chi/cors v1.1.1
 	github.com/go-chi/render v1.0.1
 	github.com/joho/godotenv v1.3.0
 	github.com/watson-developer-cloud/go-sdk v1.4.1
--- web.go
+++ web.go
@@ -5,6 +5,8 @@ import (
 	"strconv"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
 	"github.com/go-chi/render"
 	"github.com/impzero/auton/lib/ibm"
 	"github.com/impzero/auton/lib/youtube"
@@ -22,6 +24,22 @@ func NewWeb(ytClient *youtube.Client, ibmClient *ibm.Client) *Web {
 	r := chi.NewRouter()
 	w := &Web{Router: r, youtubeClient: ytClient, ibmClient: ibmClient}
 
+	r.Use(
+		middleware.RequestID,
+		middleware.RedirectSlashes,
+		middleware.RealIP,
+		middleware.Logger,
+		middleware.Recoverer,
+		Security,
+		cors.New(cors.Options{
+			AllowedOrigins:   []string{"*"},
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+			ExposedHeaders:   []string{"Link"},
+			AllowCredentials: true,
+		}).Handler,
+		middleware.Heartbeat("/ping"),
+	)
 	r.Use(render.SetContentType(render.ContentTypeJSON))
 	r.Get("/{videoID}", w.analyze)
 
@@ -53,4 +71,27 @@ func (web *Web) analyze(w http.ResponseWriter, r *http.Request) {
 
 	render.Status(r, http.StatusOK)
 	render.JSON(w, r, tones)
+}
+
+const (
+	xFrameOptions                = "X-Frame-Options"
+	xFrameOptionsValue           = "DENY"
+	xContentTypeOptions          = "X-Content-Type-Options"
+	xContentTypeOptionsValue     = "nosniff"
+	xssProtection                = "X-XSS-Protection"
+	xssProtectionValue           = "1; mode=block"
+	strictTransportSecurity      = "Strict-Transport-Security"                    // details https://blog.bracelab.com/achieving-perfect-ssl-labs-score-with-go + https://developer.mozilla.org/en-US/docs/Web/Security/HTTP_strict_transport_security
+	strictTransportSecurityValue = "max-age=31536000; includeSubDomains; preload" // 31536000 = just shy of 12 months
+	// also look at Content-Security-Policy in the future.
+)
+
+func Security(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add(xFrameOptions, xFrameOptionsValue)
+		w.Header().Add(xContentTypeOptions, xContentTypeOptionsValue)
+		w.Header().Add(xssProtection, xssProtectionValue)
+		w.Header().Add(strictTransportSecurity, strictTransportSecurityValue)
+
+		next.ServeHTTP(w, r)
+	})
 }
