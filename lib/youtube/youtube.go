b4a4408a419e7ab1e45ca96b1f8c461451eab8e1
Lyubo Lyubchev <lyubo_2317@abv.bg>
2020-05-13 19:39:03 +0300

    Revert "optimization" - actually not an optimization
    This reverts commit ff3f8f63831d8588342b85da5a8696f91e0ebe84.

--- lib/youtube/youtube.go
+++ lib/youtube/youtube.go
@@ -2,12 +2,9 @@ package youtube
 
 import (
 	"context"
+	"errors"
 	"log"
-	"runtime"
-	"sync"
-	"time"
 
-	"github.com/davecgh/go-spew/spew"
 	"google.golang.org/api/option"
 	youtube "google.golang.org/api/youtube/v3"
 )
@@ -19,8 +16,6 @@ const (
 	OrderTime      Order = "time"
 )
 
-const rateLimit = time.Second / 20
-
 type Client struct {
 	apiKey string
 }
@@ -31,73 +26,46 @@ func New(apiKey string) *Client {
 }
 
 func (c *Client) GetComments(videoId string, order Order, maxComments int) ([]string, error) {
+
 	comments := []string{}
-	youtubeService, err := youtube.NewService(context.Background(), option.WithAPIKey(c.apiKey))
+
+	ctx := context.Background()
+	ctx, cancelCtxFunc := context.WithCancel(ctx)
+
+	youtubeService, err := youtube.NewService(ctx, option.WithAPIKey(c.apiKey))
 	if err != nil {
+		cancelCtxFunc()
 		return nil, err
 	}
 
 	commentThreadsService := youtube.NewCommentThreadsService(youtubeService)
 
 	// Get the instance with which we can do api calls to the youtube api
-	apiCaller := commentThreadsService.List("snippet")
+	apiCall := commentThreadsService.List("snippet")
 
-	apiCaller.TextFormat("plainText")
-	apiCaller.VideoId(videoId)
-	apiCaller.Order(string(order))
+	apiCall.TextFormat("plainText")
+	apiCall.VideoId(videoId)
+	apiCall.Order(string(order))
 
-	var wg sync.WaitGroup
-	commentsChan := make(chan string, runtime.NumCPU())
-	quitChan := make(chan struct{})
+	err = apiCall.Pages(ctx, func(resp *youtube.CommentThreadListResponse) error {
+		for _, item := range resp.Items {
+			c := item.Snippet.TopLevelComment.Snippet.TextDisplay
+			comments = append(comments, c)
 
-	throttle := time.NewTicker(rateLimit)
+			lenComments := len(comments)
 
-	go func() {
-	forLoop:
-		for {
-			select {
-			case <-quitChan:
-				break forLoop
-			default:
-				wg.Add(1)
-
-				<-throttle.C
-				go func() {
-					defer wg.Done()
-
-					resp, err := apiCaller.Do()
-					if err != nil {
-						log.Println(err)
-						return
-					}
-
-					pageToken := resp.NextPageToken
-					if pageToken == "" {
-						quitChan <- struct{}{}
-						return
-					}
-					apiCaller.PageToken(pageToken)
-
-					for _, item := range resp.Items {
-						commentsChan <- item.Snippet.TopLevelComment.Snippet.TextDisplay
-					}
-				}()
-
+			log.Printf("%d/%d comments fetched!", lenComments, maxComments)
+			if lenComments == maxComments {
+				cancelCtxFunc()
+				break
 			}
 		}
-		wg.Wait()
-		close(commentsChan)
-		close(quitChan)
-	}()
 
-	for c := range commentsChan {
-		comments = append(comments, c)
+		return nil
+	})
 
-		spew.Dump(len(comments))
-		if len(comments) == maxComments {
-			quitChan <- struct{}{}
-			return comments, nil
-		}
+	if err != nil && !errors.Is(err, context.Canceled) {
+		return nil, err
 	}
 
 	return comments, nil
